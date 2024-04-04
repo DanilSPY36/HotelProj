7b05126a0ac4eab18565526160a8eb33232ca4e7
Danil Nazarko <danilspy36@gmail.com>
2024-04-04 20:14:35 +0300

    registration form\\ add to bd users \\ add roleid

---
+++ pages/functions.php
@@ -0,0 +1,45 @@
+<?php
+function connect(
+    $host = "localhost",
+    $user = "root",
+    $pass = "",
+    $dbname = "hotels")
+{
+    $link = new mysqli($host, $user, $pass, $dbname);
+    if($link -> connect_error) 
+    {
+        die("Error: " .$link->connect_error); // exit();
+    }
+    else
+    {
+        return $link;
+    }
+}
+
+function register($login, $email, $pass)
+{
+    $login= trim(htmlspecialchars($login));
+    $email= trim(htmlspecialchars($email));
+    $pass= trim(htmlspecialchars($pass));
+
+    if($login == "" || $email == "" || $pass == ""){
+        echo "<h3 class='text-danger text-center'> Какое-то поле не заполнено!</h3>";
+        return false;
+    }
+    if(strlen($login) < 3 || strlen($login) > 30)
+    {
+        echo "<h3 class='text-danger text-center'> Длинна логина должна быть от 3 до 30 символов </h3>";
+        return false;
+    }
+    if(strlen($pass) < 8 || strlen($pass) > 30)
+    {
+        echo "<h3 class='text-danger text-center'> Длинна пароля должна быть от 8 до 30 символов </h3>";
+        return false;
+    }
+    $ins = "insert into users(login, email, pass, roleid) values('$login','$email','$pass', 2)";
+    $link = connect();
+    $link->query($ins);
+    $link->close();
+    return true;
+}
+?>
---
+++ pages/menu.php
@@ -0,0 +1,15 @@
+<a class="nav-link text-white-50" href="index.php?page=1">Home</a>
+<a class="nav-link text-white-50" href="index.php?page=2">Hotels</a>
+<a class="nav-link text-white-50" href="index.php?page=3">Comments</a>
+<a class="nav-link text-white-50" href="index.php?page=4">Registratoin</a>
+<a class="nav-link text-white-50" href="index.php?page=5">Login</a>
+<a class="nav-link text-white-50" href="index.php?page=6">Admin</a>
+<a class="nav-link text-white-50" href="index.php?page=7">Private</a>
+<a class="nav-link text-white-50" href="index.php?page=7">Blog</a>
+
+<!-- <div class="d-flex">
+   // <?php
+   //     echo "<p>Hello, ". $_SESSION["user"]. "</p>";
+   //     echo "<button class = 'btn-outline-danger ms-2'>Exit </button>";
+   // ?>
+</div> -->
